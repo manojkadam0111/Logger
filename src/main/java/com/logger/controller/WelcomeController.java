2d42fab1ec4bcd3ad88cf64f278a94945aa28b23
manojkadam0111 <manojkadam0111@gmail.com>
2024-09-24 21:32:15 +0530

    logger added

---
+++ src/main/java/com/logger/controller/WelcomeController.java
@@ -0,0 +1,29 @@
+package com.logger.controller;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+//import org.apache.log4j.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	private static Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+	@GetMapping("/welcome")
+	public String getAllLocation() {
+		
+		String sMsg ="Start point of getAllLocation";
+		System.out.println(LocalDate.now()+""+sMsg);
+		logger.info(sMsg);
+		
+		String eMsg ="Exit point of getAllLocation";
+		System.out.println(LocalDate.now()+""+eMsg);
+		
+		return "Welcome to logger";
+		
+	}
+}
